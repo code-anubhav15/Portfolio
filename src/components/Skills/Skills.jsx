dd394f32746dcd8bd08d3080ecf9c02359af91df
Anubhav Chaudhary <anubhavch15@gmail.com>
2024-06-18 10:54:27 +0530

    Update Skills.jsx

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,30 +1,30 @@
-import React from 'react'
-import './Skills.css'
-import skillsData from './Skills_data.js'
+import React from 'react';
+import './Skills.css';
+import skillsData from './Skills_data.js';
 
 const Skills = () => {
-    return (
-      <div id='skill' className='skill'>
-        <div className="skill-title">
-          <h1>Skills</h1>
-        </div>
-        <div className="skill-content">
-          {skillsData.map((category, index) => (
-            <div className="skill-card" key={index}>
-              <h2>{category.category}</h2>
-              <div className="card-icons">
-                {category.skills.map((skill, idx) => (
-                  <div className="icons-text" data-level={skill.level} key={idx}>
-                    <img src={skill.icon} alt={skill.name} />
-                    <p>{skill.name}</p>
-                  </div>
-                ))}
-              </div>
-            </div>
-          ))}
-        </div>
+  return (
+    <div id='skill' className='skill'>
+      <div className="skill-title">
+        <h1>Skills</h1>
       </div>
-    )
-  }
+      <div className="skill-content">
+        {skillsData.map((category, index) => (
+          <div className="skill-card" key={index}>
+            <h2>{category.category}</h2>
+            <div className="card-icons">
+              {category.skills.map((skill, idx) => (
+                <div className="icons-text" data-level={skill.level} key={idx}>
+                  <img src={skill.icon} alt={skill.name} />
+                  <p>{skill.name}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
 
-export default Skills
+export default Skills;
