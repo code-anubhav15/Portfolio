3ab35695b7ca72b856b23b05b744ba146dc1b203
Anubhav Chaudhary <anubhavch15@gmail.com>
2024-06-18 10:53:56 +0530

    Update Project.jsx

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -1,22 +1,22 @@
-import React from 'react'
-import './Project.css'
-import title_pattern from '../../assets/img/theme_pattern.svg'
-import project_data from './ProjectData.js'
+import React from 'react';
+import './Project.css';
+import title_pattern from '../../assets/img/theme_pattern.svg';
+import project_data from './ProjectData.js';
 
 const Project = () => {
   return (
     <div id='project' className='project'>
       <div className="project-title">
         <h1>PROJECTS</h1>
-        <img src={title_pattern} alt=""/>
+        <img src={title_pattern} alt="Pattern"/>
       </div>
       <div className="project-container">
-        {project_data.map((project,index)=>{
-            return <img key={index} src={project.imgUrl} alt=""/>
-        })}
+        {project_data.map((project, index) => (
+          <img key={index} src={project.imgUrl} alt={`Project ${index + 1}`} />
+        ))}
       </div>
     </div>
-  )
+  );
 }
 
-export default Project
+export default Project;
